bbb16af67c53dfab624354abe81db3bf52eb9615
azadtamal <77025434+azadtamal@users.noreply.github.com>
2021-06-14 10:40:38 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+function randomColor() {
+  const colorCode = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    let num = Math.round(Math.random() * 15);
+    let num2 = colorCode[num];
+    color += num2;
+  }
+  document.getElementById("container").style.backgroundColor = color;
+  document.getElementById("output").style.color = color;
+  document.getElementById("output").style.innerHTML = color;
+}
